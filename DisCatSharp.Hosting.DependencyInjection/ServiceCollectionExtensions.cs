fc4e48c1c33492587df48cdd77aee6bcfe66d260
jbraunsmajr <jonbraunsma@gmail.com>
2021-10-28 16:21:53 -0400

    feature/multiple-bot-hosts

--- DisCatSharp.Hosting.DependencyInjection/ServiceCollectionExtensions.cs
+++ DisCatSharp.Hosting.DependencyInjection/ServiceCollectionExtensions.cs
@@ -24,5 +24,23 @@ namespace DisCatSharp.Hosting.DependencyInjection
             services.AddHostedService(provider => provider.GetRequiredService<IDiscordHostedService>());
             return services;
         }
+
+        /// <summary>
+        /// Add <typeparamref name="TService"/> as a background service which derives from
+        /// <typeparamref name="TInterface"/> and <see cref="IDiscordHostedService"/>
+        /// </summary>
+        /// <param name="services"></param>
+        /// <typeparam name="TInterface">Interface which <typeparamref name="TService"/> inherits from</typeparam>
+        /// <typeparam name="TService">Your custom bot</typeparam>
+        /// <returns></returns>
+        public static IServiceCollection AddDiscordHostedService<TInterface, TService>(this IServiceCollection services)
+            where TInterface :  class, IDiscordHostedService
+            where TService : class, TInterface, IDiscordHostedService
+        {
+            services.AddSingleton<TInterface, TService>();
+            services.AddHostedService(provider => provider.GetRequiredService<TInterface>());
+
+            return services;
+        }
     }
 }
