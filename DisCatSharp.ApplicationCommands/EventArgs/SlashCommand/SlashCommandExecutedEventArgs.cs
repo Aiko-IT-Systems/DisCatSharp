113ceee97217e262bf0365f71f7d8e1cebd726f0
Lala Sabathil <aiko@aitsys.dev>
2021-11-04 19:09:57 +0100

    Extend AutocompleteContext

--- DisCatSharp.ApplicationCommands/EventArgs/SlashCommand/SlashCommandExecutedEventArgs.cs
+++ DisCatSharp.ApplicationCommands/EventArgs/SlashCommand/SlashCommandExecutedEventArgs.cs
@@ -36,6 +36,10 @@ namespace DisCatSharp.ApplicationCommands.EventArgs
         /// </summary>
         public InteractionContext Context { get; internal set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SlashCommandExecutedEventArgs"/> class.
+        /// </summary>
+        /// <param name="provider">The provider.</param>
         public SlashCommandExecutedEventArgs(IServiceProvider provider) : base(provider)
         { }
     }
