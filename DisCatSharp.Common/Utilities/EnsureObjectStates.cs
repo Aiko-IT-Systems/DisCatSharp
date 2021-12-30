6b1351739f5b8499c6739cc21f76a866e240a74a
Lala Sabathil <aiko@aitsys.dev>
2021-12-30 17:12:25 +0100

    Update EnsureObjectStates.cs

--- DisCatSharp.Common/Utilities/EnsureObjectStates.cs
+++ DisCatSharp.Common/Utilities/EnsureObjectStates.cs
@@ -33,45 +33,45 @@ namespace DisCatSharp.Common
         /// <summary>
         /// Checks whether the dictionary is null or empty.
         /// </summary>
-        /// <typeparam name="T1">Any.</typeparam>
-        /// <typeparam name="T2">Any.</typeparam>
+        /// <typeparam name="T1">Any key type.</typeparam>
+        /// <typeparam name="T2">Any value type.</typeparam>
         /// <param name="dictionary">The dictionary to check on.</param>
         /// <returns>True if satisfied, false otherwise.</returns>
         #nullable enable
         public static bool EmptyOrNull<T1, T2>(this Dictionary<T1?, T2?>? dictionary)
             => dictionary == null || !dictionary.Any() || dictionary.Keys == null || !dictionary.Keys.Any();
-        #nullable disable
+#nullable disable
 
         /// <summary>
         /// Checks whether the dictionary is not null and not empty.
         /// </summary>
-        /// <typeparam name="T1">Any.</typeparam>
-        /// <typeparam name="T2">Any.</typeparam>
+        /// <typeparam name="T1">Any key type.</typeparam>
+        /// <typeparam name="T2">Any value type.</typeparam>
         /// <param name="dictionary">The dictionary to check on.</param>
         /// <returns>True if satisfied, false otherwise.</returns>
-        #nullable enable
+#nullable enable
         public static bool NotEmptyAndNotNull<T1, T2>(this Dictionary<T1?, T2?>? dictionary)
             => dictionary != null && dictionary.Any() && dictionary.Keys != null && dictionary.Keys.Any();
-        #nullable disable
+#nullable disable
 
         /// <summary>
         /// Checks whether the list is null or empty.
         /// </summary>
-        /// <typeparam name="T">Any.</typeparam>
+        /// <typeparam name="T">Any value type.</typeparam>
         /// <param name="list">The list to check on.</param>
         /// <returns>True if satisfied, false otherwise.</returns>
-        #nullable enable
+#nullable enable
         public static bool EmptyOrNull<T>(this List<T?>? list)
             => list == null || !list.Any();
-        #nullable disable
+#nullable disable
 
         /// <summary>
         /// Checks whether the list is not null and not empty.
         /// </summary>
-        /// <typeparam name="T">Any.</typeparam>
+        /// <typeparam name="T">Any value type.</typeparam>
         /// <param name="list">The list to check on.</param>
         /// <returns>True if satisfied, false otherwise.</returns>
-        #nullable enable
+#nullable enable
         public static bool NotEmptyAndNotNull<T>(this List<T?>? list)
             => list != null && list.Any();
         #nullable disable
