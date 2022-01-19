a7a7c97d0ff7a66b0ee2be2dc17988f4f93037d0
Lala Sabathil <aiko@aitsys.dev>
2022-01-19 17:17:12 +0100

    Update DisCatSharp/Entities/Guild/ScheduledEvent/DiscordScheduledEvent.cs

--- DisCatSharp/Entities/Guild/ScheduledEvent/DiscordScheduledEvent.cs
+++ DisCatSharp/Entities/Guild/ScheduledEvent/DiscordScheduledEvent.cs
@@ -109,7 +109,6 @@ namespace DisCatSharp.Entities
         [JsonIgnore]
         public string CoverImageUrl
             => !string.IsNullOrWhiteSpace(this.CoverImageHash) ? $"{DiscordDomain.GetDomain(CoreDomain.DiscordCdn).Uri}{Endpoints.GUILD_EVENTS}/{this.Id.ToString(CultureInfo.InvariantCulture)}/{this.CoverImageHash}.png" : null;
-		// Somehow they does not include a extension. .{(this.CoverImageHash.StartsWith("a_") ? "gif" : "png")}
 
 		/// <summary>
 		/// Gets the scheduled start time of the scheduled event.
