5beeb6b152cc52639c3c7fc0ae7582c5343b8ba2
Lala Sabathil <aiko@aitsys.dev>
2022-01-29 03:17:35 +0100

    It's ulong not int..

--- DisCatSharp/Entities/Interaction/DiscordInteractionDataOption.cs
+++ DisCatSharp/Entities/Interaction/DiscordInteractionDataOption.cs
@@ -75,7 +75,7 @@ namespace DisCatSharp.Entities
 						ApplicationCommandOptionType.Role => ulong.Parse(this.RawValue),
 						ApplicationCommandOptionType.Mentionable => ulong.Parse(this.RawValue),
 						ApplicationCommandOptionType.Number => double.Parse(this.RawValue),
-						ApplicationCommandOptionType.Attachment => int.Parse(this.RawValue),
+						ApplicationCommandOptionType.Attachment => ulong.Parse(this.RawValue),
 						_ => this.RawValue,
 					};
 
