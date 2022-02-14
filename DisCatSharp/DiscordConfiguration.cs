58d2ec31e4135fbdb814a27ab729c73b15154872
J_M_Lutra <64010750+JMLutra@users.noreply.github.com>
2022-02-14 18:25:07 +0100

    changed default to v10

--- DisCatSharp/DiscordConfiguration.cs
+++ DisCatSharp/DiscordConfiguration.cs
@@ -68,7 +68,7 @@ namespace DisCatSharp
 		/// Overwrites the api version.
 		/// Defaults to 9.
 		/// </summary>
-		public string ApiVersion { internal get; set; } = "9";
+		public string ApiVersion { internal get; set; } = "10";
 
 		/// <summary>
 		/// <para>Sets whether to rely on Discord for NTP (Network Time Protocol) synchronization with the "X-Ratelimit-Reset-After" header.</para>
