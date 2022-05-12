4bea2d304d601b04a03e25267b49193c31585b76
Mira <56395159+TheXorog@users.noreply.github.com>
2022-05-12 20:49:04 +0200

    feat: Paginated Modals (#134)
    Closes T142

--- DisCatSharp.Interactivity/Extensions/InteractionExtensions.cs
+++ DisCatSharp.Interactivity/Extensions/InteractionExtensions.cs
@@ -20,11 +20,13 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 using DisCatSharp.Entities;
+using DisCatSharp.EventArgs;
 using DisCatSharp.Interactivity.Enums;
 using DisCatSharp.Interactivity.EventHandling;
 
@@ -51,5 +53,70 @@ namespace DisCatSharp.Interactivity.Extensions
 		/// <param name="token">A custom cancellation token that can be cancelled at any point.</param>
 		public static Task SendPaginatedResponseAsync(this DiscordInteraction interaction, bool ephemeral, DiscordUser user, IEnumerable<Page> pages, PaginationButtons buttons = null, PaginationBehaviour? behaviour = default, ButtonPaginationBehavior? deletion = default, CancellationToken token = default)
 			=> MessageExtensions.GetInteractivity(interaction.Message).SendPaginatedResponseAsync(interaction, ephemeral, user, pages, buttons, behaviour, deletion, token);
+
+		/// <summary>
+		/// Sends multiple modals to the user with a prompt to open the next one.
+		/// </summary>
+		/// <param name="interaction">The interaction to create a response to.</param>
+		/// <param name="modals">The modal pages.</param>
+		/// <param name="timeOutOverride">A custom timeout. (Default: 15 minutes)</param>
+		/// <returns>A read-only dictionary with the customid of the components as the key.</returns>
+		/// <exception cref="ArgumentException">Is thrown when no modals are defined.</exception>
+		/// <exception cref="InvalidOperationException">Is thrown when interactivity is not enabled for the client/shard.</exception>
+		public static async Task<PaginatedModalResponse> CreatePaginatedModalResponseAsync(this DiscordInteraction interaction, IReadOnlyList<ModalPage> modals, TimeSpan? timeOutOverride = null)
+		{
+			if (modals is null || modals.Count == 0)
+				throw new ArgumentException("You have to set at least one page");
+
+			var client = (DiscordClient)interaction.Discord;
+			var interactivity = client.GetInteractivity() ?? throw new InvalidOperationException($"Interactivity is not enabled for this {(client.IsShard ? "shard" : "client")}.");
+
+			timeOutOverride ??= TimeSpan.FromMinutes(15);
+
+			Dictionary<string, string> caughtResponses = new();
+
+			var previousInteraction = interaction;
+
+			foreach (var b in modals)
+			{
+				var modal = b.Modal.WithCustomId(Guid.NewGuid().ToString());
+
+				if (previousInteraction.Type is InteractionType.Ping or InteractionType.ModalSubmit)
+				{
+					await previousInteraction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, b.OpenMessage.AddComponents(b.OpenButton));
+					var originalResponse = await previousInteraction.GetOriginalResponseAsync();
+					var modalOpen = await interactivity.WaitForButtonAsync(originalResponse, new List<DiscordButtonComponent> { b.OpenButton }, timeOutOverride);
+
+					if (modalOpen.TimedOut)
+					{
+						_ = previousInteraction.EditOriginalResponseAsync(new DiscordWebhookBuilder().WithContent(b.OpenMessage.Content).AddComponents(b.OpenButton.Disable()));
+						return new PaginatedModalResponse { TimedOut = true };
+					}
+
+					await modalOpen.Result.Interaction.CreateInteractionModalResponseAsync(modal);
+				}
+				else
+				{
+					await previousInteraction.CreateInteractionModalResponseAsync(modal);
+				}
+
+				_ = previousInteraction.EditOriginalResponseAsync(new DiscordWebhookBuilder().WithContent(b.OpenMessage.Content).AddComponents(b.OpenButton.Disable()));
+
+				var modalResult = await interactivity.WaitForModalAsync(modal.CustomId, timeOutOverride);
+
+				if (modalResult.TimedOut)
+					return new PaginatedModalResponse { TimedOut = true };
+
+				foreach (var row in modalResult.Result.Interaction.Data.Components)
+					foreach (var submissions in row.Components)
+						caughtResponses.Add(submissions.CustomId, submissions.Value);
+
+				previousInteraction = modalResult.Result.Interaction;
+			}
+
+			await previousInteraction.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource, new DiscordInteractionResponseBuilder().AsEphemeral());
+
+			return new PaginatedModalResponse { TimedOut = false, Responses = caughtResponses, Interaction = previousInteraction };
+		}
 	}
 }
