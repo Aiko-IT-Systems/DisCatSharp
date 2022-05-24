d42e442607c25d862274ffbc530c32fdf26c4814
Lala Sabathil <aiko@aitsys.dev>
2022-05-24 06:08:45 +0200

    [skip appveyor] Update theme

--- DisCatSharp.Docs/dcs/common.js
+++ DisCatSharp.Docs/dcs/common.js
@@ -25,6 +25,7 @@ function getDirectoryName(path) {
 }
 
 function getHtmlId(input) {
+    if (!input) return '';
     return input.replace(/\W/g, '_');
 }
 
@@ -69,6 +70,7 @@ var gitUrlPatternItems = {
         'testRegex': /^(https?:\/\/)?(\S+\@)?(\S+\.)?github\.com(\/|:).*/i,
         'generateUrl': function (gitInfo) {
             var url = normalizeGitUrlToHttps(gitInfo.repo);
+            url = getRepoWithoutGitExtension(url);
             url += '/blob' + '/' + gitInfo.branch + '/' + gitInfo.path;
             if (gitInfo.startLine && gitInfo.startLine > 0) {
                 url += '/#L' + gitInfo.startLine;
@@ -77,6 +79,7 @@ var gitUrlPatternItems = {
         },
         'generateNewFileUrl': function (gitInfo, uid) {
             var url = normalizeGitUrlToHttps(gitInfo.repo);
+            url = getRepoWithoutGitExtension(url);
             url += '/new';
             url += '/' + gitInfo.branch;
             url += '/' + getOverrideFolder(gitInfo.apiSpecFolder);
@@ -86,13 +89,18 @@ var gitUrlPatternItems = {
         }
     },
     'vso': {
+        // HTTPS form: https://{account}@dev.azure.com/{account}/{project}/_git/{repo}
         // HTTPS form: https://{user}.visualstudio.com/{org}/_git/{repo}
+        // SSH form: git@ssh.dev.azure.com:v3/{account}/{project}/{repo}
         // SSH form: ssh://{user}@{user}.visualstudio.com:22/{org}/_git/{repo}
-        // generated URL: https://{user}.visualstudio.com/{org}/_git/{repo}?path={path}&version=GB{branch}
-        'testRegex': /^(https?:\/\/)?(ssh:\/\/\S+\@)?(\S+\.)?visualstudio\.com(\/|:).*/i,
+        // generated URL under branch: https://{account}@dev.azure.com/{account}/{project}/_git/{repo}?version=GB{branch}
+        // generated URL under branch: https://{user}.visualstudio.com/{org}/_git/{repo}?path={path}&version=GB{branch}
+        // generated URL under detached HEAD: https://{user}.visualstudio.com/{org}/_git/{repo}?path={path}&version=GC{commit}
+        'testRegex': /^(https?:\/\/)?(ssh:\/\/\S+\@)?(\S+@)?(\S+\.)?(dev\.azure|visualstudio)\.com(\/|:).*/i,
         'generateUrl': function (gitInfo) {
             var url = normalizeGitUrlToHttps(gitInfo.repo);
-            url += '?path=' + gitInfo.path + '&version=GB' + gitInfo.branch;
+            var branchPrefix = /[0-9a-fA-F]{40}/.test(gitInfo.branch) ? 'GC' : 'GB';
+            url += '?path=' + gitInfo.path + '&version=' + branchPrefix + gitInfo.branch;
             if (gitInfo.startLine && gitInfo.startLine > 0) {
                 url += '&line=' + gitInfo.startLine;
             }
@@ -101,7 +109,32 @@ var gitUrlPatternItems = {
         'generateNewFileUrl': function (gitInfo, uid) {
             return '';
         }
+    },
+    'bitbucket': {
+        // HTTPS form: https://{user}@bitbucket.org/{org}/{repo}.git
+        // SSH form: git@bitbucket.org:{org}/{repo}.git
+        // generate URL: https://bitbucket.org/{org}/{repo}/src/{branch}/{path}
+        'testRegex': /^(https?:\/\/)?(\S+\@)?(\S+\.)?bitbucket\.org(\/|:).*/i,
+        'generateUrl': function (gitInfo) {
+            var url = normalizeGitUrlToHttps(gitInfo.repo);
+            url = getRepoWithoutGitExtension(url);
+            url += '/src' + '/' + gitInfo.branch + '/' + gitInfo.path;
+            if (gitInfo.startLine && gitInfo.startLine > 0) {
+                url += '#lines-' + gitInfo.startLine;
+            }
+            return url;
+        },
+        'generateNewFileUrl': function (gitInfo, uid) {
+            return '';
+        }
     }
+}
+
+function getRepoWithoutGitExtension(repo) {
+    if (repo.substr(-4) === '.git') {
+        repo = repo.substr(0, repo.length - 4);
+    }
+    return repo;
 }
 
 function normalizeGitUrlToHttps(repo) {
@@ -121,10 +154,6 @@ function getNewFileUrl(item, gitContribute, gitUrlPattern) {
         return '';
     }
 
-    if (gitInfo.repo.substr(-4) === '.git') {
-        gitInfo.repo = gitInfo.repo.substr(0, gitInfo.repo.length - 4);
-    }
-
     var patternName = getPatternName(gitInfo.repo, gitUrlPattern);
     if (!patternName) return patternName;
     return gitUrlPatternItems[patternName].generateNewFileUrl(gitInfo, item.uid);
@@ -136,10 +165,6 @@ function getRemoteUrl(remote, startLine, gitContribute, gitUrlPattern) {
         return '';
     }
 
-    if (gitInfo.repo.substr(-4) === '.git') {
-        gitInfo.repo = gitInfo.repo.substr(0, gitInfo.repo.length - 4);
-    }
-
     var patternName = getPatternName(gitInfo.repo, gitUrlPattern);
     if (!patternName) return '';
 
@@ -149,29 +174,26 @@ function getRemoteUrl(remote, startLine, gitContribute, gitUrlPattern) {
 
 function getGitInfo(gitContribute, gitRemote) {
     // apiSpecFolder defines the folder contains overwrite files for MRef, the default value is apiSpec
-    var defaultApiSpecFolder = "apiSpec";
+    var defaultApiSpecFolder = 'apiSpec';
 
-    if (!gitContribute && !gitRemote) {
-        return {
-            apiSpecFolder: defaultApiSpecFolder
-        }
+    var result = {};
+    if (gitContribute && gitContribute.apiSpecFolder) {
+        result.apiSpecFolder = gitContribute.apiSpecFolder;
+    } else {
+        result.apiSpecFolder = defaultApiSpecFolder;
     }
+    mergeKey(gitContribute, gitRemote, result, 'repo');
+    mergeKey(gitContribute, gitRemote, result, 'branch');
+    mergeKey(gitContribute, gitRemote, result, 'path');
 
-    if (!gitContribute) {
-        return {
-            apiSpecFolder: defaultApiSpecFolder,
-            repo: gitRemote.repo,
-            branch: gitRemote.branch,
-            // path defines the relative path from current git repo.
-            path: gitRemote.path
+    return result;
+
+    function mergeKey(source, sourceFallback, dest, key) {
+        if (source && source.hasOwnProperty(key)) {
+            dest[key] = source[key];
+        } else if (sourceFallback && sourceFallback.hasOwnProperty(key)) {
+            dest[key] = sourceFallback[key];
         }
-    }
-
-    return {
-        apiSpecFolder: gitContribute.apiSpecFolder || defaultApiSpecFolder,
-        repo: gitContribute.repo || gitRemote.repo,
-        branch: gitContribute.branch || gitRemote.branch,
-        path: gitRemote.path
     }
 }
 
@@ -190,7 +212,7 @@ function getPatternName(repo, gitUrlPattern) {
 
 function getOverrideFolder(path) {
     if (!path) return "";
-    path = path.replace('\\', '/');
+    path = path.replace(/\\/g, '/');
     if (path.charAt(path.length - 1) == '/') path = path.substring(0, path.length - 1);
     return path;
 }
