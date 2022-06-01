e65e4ab9680e35e972d2e83f0a45801b1698a65d
Lala Sabathil <dcs@aitsys.dev>
2022-06-01 05:35:48 +0200

    Update EnsureObjectStates.cs

--- DisCatSharp.Common/Utilities/EnsureObjectStates.cs
+++ DisCatSharp.Common/Utilities/EnsureObjectStates.cs
@@ -33,44 +33,38 @@ public static class EnsureObjectStates
 	/// <summary>
 	/// Checks whether the dictionary is null or empty.
 	/// </summary>
-	public static class EnsureObjectStates
-	{
-		/// <summary>
-		/// Checks whether the dictionary is null or empty.
-		/// </summary>
-		/// <typeparam name="T1">Any key type.</typeparam>
-		/// <typeparam name="T2">Any value type.</typeparam>
-		/// <param name="dictionary">The dictionary to check on.</param>
-		/// <returns>True if satisfied, false otherwise.</returns>
-		public static bool EmptyOrNull<T1, T2>(this Dictionary<T1, T2?>? dictionary) where T1 : notnull
-			=> dictionary == null || !dictionary.Any() || !dictionary.Keys.Any();
+	/// <typeparam name="T1">Any key type.</typeparam>
+	/// <typeparam name="T2">Any value type.</typeparam>
+	/// <param name="dictionary">The dictionary to check on.</param>
+	/// <returns>True if satisfied, false otherwise.</returns>
+	public static bool EmptyOrNull<T1, T2>(this Dictionary<T1, T2?>? dictionary) where T1 : notnull
+		=> dictionary == null || !dictionary.Any() || !dictionary.Keys.Any();
 
-		/// <summary>
-		/// Checks whether the dictionary is not null and not empty.
-		/// </summary>
-		/// <typeparam name="T1">Any key type.</typeparam>
-		/// <typeparam name="T2">Any value type.</typeparam>
-		/// <param name="dictionary">The dictionary to check on.</param>
-		/// <returns>True if satisfied, false otherwise.</returns>
-		public static bool NotEmptyAndNotNull<T1, T2>(this Dictionary<T1, T2?>? dictionary) where T1 : notnull
-			=> dictionary != null && dictionary.Any() && dictionary.Keys.Any();
+	/// <summary>
+	/// Checks whether the dictionary is not null and not empty.
+	/// </summary>
+	/// <typeparam name="T1">Any key type.</typeparam>
+	/// <typeparam name="T2">Any value type.</typeparam>
+	/// <param name="dictionary">The dictionary to check on.</param>
+	/// <returns>True if satisfied, false otherwise.</returns>
+	public static bool NotEmptyAndNotNull<T1, T2>(this Dictionary<T1, T2?>? dictionary) where T1 : notnull
+		=> dictionary != null && dictionary.Any() && dictionary.Keys.Any();
 
-		/// <summary>
-		/// Checks whether the list is null or empty.
-		/// </summary>
-		/// <typeparam name="T">Any value type.</typeparam>
-		/// <param name="list">The list to check on.</param>
-		/// <returns>True if satisfied, false otherwise.</returns>
-		public static bool EmptyOrNull<T>(this List<T?>? list)
-			=> list == null || !list.Any();
+	/// <summary>
+	/// Checks whether the list is null or empty.
+	/// </summary>
+	/// <typeparam name="T">Any value type.</typeparam>
+	/// <param name="list">The list to check on.</param>
+	/// <returns>True if satisfied, false otherwise.</returns>
+	public static bool EmptyOrNull<T>(this List<T?>? list)
+		=> list == null || !list.Any();
 
-		/// <summary>
-		/// Checks whether the list is not null and not empty.
-		/// </summary>
-		/// <typeparam name="T">Any value type.</typeparam>
-		/// <param name="list">The list to check on.</param>
-		/// <returns>True if satisfied, false otherwise.</returns>
-		public static bool NotEmptyAndNotNull<T>(this List<T?>? list)
-			=> list != null && list.Any();
-	}
+	/// <summary>
+	/// Checks whether the list is not null and not empty.
+	/// </summary>
+	/// <typeparam name="T">Any value type.</typeparam>
+	/// <param name="list">The list to check on.</param>
+	/// <returns>True if satisfied, false otherwise.</returns>
+	public static bool NotEmptyAndNotNull<T>(this List<T?>? list)
+		=> list != null && list.Any();
 }
