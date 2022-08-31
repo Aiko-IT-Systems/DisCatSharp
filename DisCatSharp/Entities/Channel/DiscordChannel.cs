d0c3cf7ce53b387cd0bb2c6cfa026f9a799a9281
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-08-31 03:08:28 +0200

    Properly utilizes DiscordThreadChannel's inheritance

--- DisCatSharp/Entities/Channel/DiscordChannel.cs
+++ DisCatSharp/Entities/Channel/DiscordChannel.cs
@@ -127,7 +127,7 @@ public class DiscordChannel : SnowflakeObject, IEquatable<DiscordChannel>
 	/// </summary>
 	[JsonIgnore]
 	public bool IsPrivate
-		=> this.Type == ChannelType.Private || this.Type == ChannelType.Group;
+		=> this.Type is ChannelType.Private or ChannelType.Group or ChannelType.PrivateThread;
 
 	/// <summary>
 	/// Gets whether this channel is a channel category.
