4dca8081783d46e1664e86838cef439bf42bd9a0
Salvage <29021710+Saalvage@users.noreply.github.com>
2022-08-31 03:32:56 +0200

    Fix `IsPrivate`

--- DisCatSharp/Entities/Channel/DiscordChannel.cs
+++ DisCatSharp/Entities/Channel/DiscordChannel.cs
@@ -127,7 +127,7 @@ public class DiscordChannel : SnowflakeObject, IEquatable<DiscordChannel>
 	/// </summary>
 	[JsonIgnore]
 	public bool IsPrivate
-		=> this.Type is ChannelType.Private or ChannelType.Group or ChannelType.PrivateThread;
+		=> this.Type is ChannelType.Private or ChannelType.Group;
 
 	/// <summary>
 	/// Gets whether this channel is a channel category.
