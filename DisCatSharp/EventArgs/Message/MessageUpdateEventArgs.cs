35de99d27be2a32cdb677ab0040ab462b967c6fe
Lala Sabathil <aiko@aitsys.dev>
2022-09-14 13:12:44 +0200

    Revert "Fix 1018018990519496734"
    This reverts commit 90d03b1a45bc622242dd0b04d3916e74ef87936c.

--- DisCatSharp/EventArgs/Message/MessageUpdateEventArgs.cs
+++ DisCatSharp/EventArgs/Message/MessageUpdateEventArgs.cs
@@ -84,8 +84,5 @@ public class MessageUpdateEventArgs : DiscordEventArgs
 	/// <summary>
 	/// Initializes a new instance of the <see cref="MessageUpdateEventArgs"/> class.
 	/// </summary>
-	internal MessageUpdateEventArgs(IServiceProvider provider) : base(provider)
-	{
-		this.Message.Channel.GuildId = this.Message.GuildId;
-	}
+	internal MessageUpdateEventArgs(IServiceProvider provider) : base(provider) { }
 }
