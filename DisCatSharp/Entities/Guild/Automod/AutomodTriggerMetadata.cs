c3f40a18fa402c7fd8fb40d297ced183fd4af350
maxximum <maxximum2006@gmail.com>
2022-11-03 17:30:29 +0100

    fix automodtriggermetadata

--- DisCatSharp/Entities/Guild/Automod/AutomodTriggerMetadata.cs
+++ DisCatSharp/Entities/Guild/Automod/AutomodTriggerMetadata.cs
@@ -20,6 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 using DisCatSharp.Enums;
