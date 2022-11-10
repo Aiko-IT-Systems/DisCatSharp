487d1284e5aa5cd7efcefce5366b10ce01553d55
Lala Sabathil <aiko@aitsys.dev>
2022-11-10 20:59:40 +0100

    feat: Add ApplicationFlags
    Add:
    - RpcPrivateBeta (1<<5)
    - AllowAssets (1<<8)
    - AllowActivityActionSpectate (1<<9)
    - AllowActivityActionJoinRequest (1<<10)
    - ActiveApplication (1<<24)

--- DisCatSharp/Enums/Application/ApplicationFlags.cs
+++ DisCatSharp/Enums/Application/ApplicationFlags.cs
@@ -28,81 +28,106 @@ namespace DisCatSharp.Enums;
 /// Represents additional details of an application.
 /// </summary>
 [Flags]
-public enum ApplicationFlags
+public enum ApplicationFlags : long
 {
 	/// <summary>
 	/// The application is embedded and can be used by users.
 	/// This was introduced to avoid users using in-dev apps.
 	/// </summary>
-	EmbeddedReleased = 1 << 1,
+	EmbeddedReleased = 1L << 1,
 
 	/// <summary>
 	/// The application is a managed emoji.
 	/// </summary>
-	ManagedEmoji = 1 << 2,
+	ManagedEmoji = 1L << 2,
 
 	/// <summary>
 	/// The application can create group dms.
 	/// </summary>
-	GroupDmCreate = 1 << 5,
+	GroupDmCreate = 1L << 5,
+
+	/// <summary>
+	/// Allows the application to access the local RPC server.
+	/// </summary>
+	RpcPrivateBeta = 1L << 5,
+
+	/// <summary>
+	/// Allows the application to create activity assets.
+	/// </summary>
+	AllowAssets = 1L<<8,
+
+	/// <summary>
+	/// Allows the application to enable activity spectating.
+	/// </summary>
+	AllowActivityActionSpectate = 1L<<9,
+
+	/// <summary>
+	/// Allows the application to enable join requests for activities.
+	/// </summary>
+	AllowActivityActionJoinRequest = 1L<<10,
 
 	/// <summary>
 	/// The application has connected to RPC.
 	/// </summary>
-	RpcHasConnected = 1 << 11,
+	RpcHasConnected = 1L << 11,
 
 	/// <summary>
 	/// The application can track presence data.
 	/// </summary>
-	GatewayPresence = 1 << 12,
+	GatewayPresence = 1L << 12,
 
 	/// <summary>
 	/// The application can track presence data (limited).
 	/// </summary>
-	GatewayPresenceLimited = 1 << 13,
+	GatewayPresenceLimited = 1L << 13,
 
 	/// <summary>
 	/// The application can track guild members.
 	/// </summary>
-	GatewayGuildMembers = 1 << 14,
+	GatewayGuildMembers = 1L << 14,
 
 	/// <summary>
 	/// The application can track guild members (limited).
 	/// </summary>
-	GatewayGuildMembersLimited = 1 << 15,
+	GatewayGuildMembersLimited = 1L << 15,
 
 	/// <summary>
 	/// The application can track pending guild member verifications (limited).
 	/// </summary>
-	VerificationPendingGuildLimit = 1 << 16,
+	VerificationPendingGuildLimit = 1L << 16,
 
 	/// <summary>
 	/// The application is embedded.
 	/// </summary>
-	Embedded = 1 << 17,
+	Embedded = 1L << 17,
 
 	/// <summary>
 	/// The application can track message content.
 	/// </summary>
-	GatewayMessageContent = 1 << 18,
+	GatewayMessageContent = 1L << 18,
 
 	/// <summary>
 	/// The application can track message content (limited).
 	/// </summary>
-	GatewayMessageContentLimited = 1 << 19,
+	GatewayMessageContentLimited = 1L << 19,
 
 	/// <summary>
 	/// Related to embedded applications.
 	/// </summary>
-	EmbeddedFirstParty = 1 << 20,
+	EmbeddedFirstParty = 1L << 20,
 
 	/// <summary>
 	/// To be datamined.
 	/// </summary>
-	UnknownFlag = 1 << 21,
+	UnknownFlag = 1L << 21,
 
 	/// <summary>
 	/// The application has registered global application commands.
 	/// </summary>
-	ApplicationCommandBadge = 1 << 23
+	ApplicationCommandBadge = 1L << 23,
+
+	/// <summary>
+	/// Indicates if an app is considered active. This means that it has had any global command executed in the past 30 days.
+	/// </summary>
+	ActiveApplication = 1L << 24
 }
