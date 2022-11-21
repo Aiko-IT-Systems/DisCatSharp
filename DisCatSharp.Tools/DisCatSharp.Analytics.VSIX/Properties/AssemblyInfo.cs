47d3b6af1e6099f6dfb9e6159f173292b6e75244
Lala Sabathil <aiko@aitsys.dev>
2022-11-21 08:30:03 +0100

    stuff

---
+++ DisCatSharp.Tools/DisCatSharp.Analytics.VSIX/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using DisCatSharp.Analytics.VSIX;
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DisCatSharp Analytics")]
+[assembly: AssemblyDescription("DisCatSharp Analytics Extension")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AITSYS")]
+[assembly: AssemblyProduct("DisCatSharp.Analytics")]
+[assembly: AssemblyCopyright("2022 AITSYS")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
+
+namespace System.Runtime.CompilerServices
+{
+	public class IsExternalInit { }
+}
