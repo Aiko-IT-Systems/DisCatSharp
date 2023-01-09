830538a6a097175cecde2001001702ca4c037dcf
Mira <mira@aitsys.dev>
2023-01-09 23:12:10 +0100

    Update DisCatSharp/Entities/Guild/ThreadAndForum/DiscordThreadChannel.cs
    Co-authored-by: Lala Sabathil <aiko@aitsys.dev>
    Signed-off-by: Mira <xorog@outlook.de>

--- DisCatSharp/Entities/Guild/ThreadAndForum/DiscordThreadChannel.cs
+++ DisCatSharp/Entities/Guild/ThreadAndForum/DiscordThreadChannel.cs
@@ -259,7 +259,7 @@ public class DiscordThreadChannel : DiscordChannel
 	/// Tries to get a member in this thread.
 	/// </summary>
 	/// <param name="member">The member to get.</param>
-	/// <param name="withMember">Whether to request the member object. (If set to true, will paginate the result)</param>
+	/// <param name="withMember">Whether to request the member object.</param>
 	/// <exception cref="BadRequestException">Thrown when an invalid parameter was provided.</exception>
 	/// <exception cref="ServerErrorException">Thrown when Discord is unable to process the request.</exception>
 #pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
