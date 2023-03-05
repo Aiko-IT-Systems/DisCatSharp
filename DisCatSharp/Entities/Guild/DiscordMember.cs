7bc3128b00ea33476a22c10ec75a2b01635f684c
Lala Sabathil <aiko@aitsys.dev>
2023-03-05 19:39:31 +0100

    fix! Display name breaks

--- DisCatSharp/Entities/Guild/DiscordMember.cs
+++ DisCatSharp/Entities/Guild/DiscordMember.cs
@@ -665,7 +665,7 @@ public class DiscordMember : DiscordUser, IEquatable<DiscordMember>
 	/// </summary>
 	/// <returns>String representation of this member.</returns>
 	public override string ToString()
-		=> $"Member {this.Id}; {this.Username}#{this.Discriminator} ({this.DisplayName})";
+		=> $"Member {this.Id}; {this.Username}#{this.Discriminator}";
 
 	/// <summary>
 	/// Checks whether this <see cref="DiscordMember"/> is equal to another object.
