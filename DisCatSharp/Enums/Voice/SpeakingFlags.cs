38cd803d20efc527b22b5df512b5de6a039808a3
Lala Sabathil <aiko@aitsys.dev>
2023-03-07 05:05:31 +0100

    fix: speaking is flags not bool (#276)

--- DisCatSharp/Enums/Voice/SpeakingFlags.cs
+++ DisCatSharp/Enums/Voice/SpeakingFlags.cs
@@ -20,35 +20,27 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using System;
+namespace DisCatSharp.Enums;
 
-using DisCatSharp.Entities;
-using DisCatSharp.Enums;
-
-namespace DisCatSharp.EventArgs;
-
-/// <summary>
-/// Represents arguments for UserSpeaking event.
-/// </summary>
-public class UserSpeakingEventArgs : DiscordEventArgs
+public enum SpeakingFlags : int
 {
 	/// <summary>
-	/// Gets the users whose speaking state changed.
+	/// Not speaking.
 	/// </summary>
-	public DiscordUser User { get; internal set; }
+	NotSpeaking = 0,
 
 	/// <summary>
-	/// Gets the SSRC of the audio source.
+	/// Normal transmission of voice audio.
 	/// </summary>
-	public uint Ssrc { get; internal set; }
+	Microphone = 1<<0,
 
 	/// <summary>
-	/// Gets whether this user is speaking.
+	/// Transmission of context audio for video, no speaking indicator.
 	/// </summary>
-	public SpeakingFlags Speaking { get; internal set; }
+	Soundshare = 1<<1,
 
 	/// <summary>
-	/// Initializes a new instance of the <see cref="UserSpeakingEventArgs"/> class.
+	/// Priority speaker, lowering audio of other speakers.
 	/// </summary>
-	internal UserSpeakingEventArgs(IServiceProvider provider) : base(provider) { }
+	Priority = 1<<2
 }
