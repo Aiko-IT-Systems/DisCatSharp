3fb047bc73c13617d7fa8113e00f74163b3a9cda
Lala Sabathil <aiko@aitsys.dev>
2023-03-09 01:13:09 +0100

    fix(app_com): flag fuckery

--- DisCatSharp/Entities/Interaction/DiscordFollowupMessageBuilder.cs
+++ DisCatSharp/Entities/Interaction/DiscordFollowupMessageBuilder.cs
@@ -40,17 +40,44 @@ public sealed class DiscordFollowupMessageBuilder
 	/// <summary>
 	/// Whether this followup message should be ephemeral.
 	/// </summary>
-	public bool IsEphemeral { get; set; }
+	public bool IsEphemeral
+	{
+		get => this.EPH;
+		set
+		{
+			this.EPH = value;
+			this._flagsChanged = true;
+		}
+	}
+	private bool EPH { get; set; }
 
 	/// <summary>
 	/// Whether to suppress embeds.
 	/// </summary>
-	public bool EmbedsSuppressed { get; set; }
+	public bool EmbedsSuppressed
+	{
+		get => this.EMB_SUP;
+		set
+		{
+			this.EMB_SUP = value;
+			this._flagsChanged = true;
+		}
+	}
+	private bool EMB_SUP { get; set; }
 
 	/// <summary>
 	/// Whether to send as silent message.
 	/// </summary>
-	public bool NotificationsSuppressed { get; set; }
+	public bool NotificationsSuppressed
+	{
+		get => this.NOTI_SUP;
+		set
+		{
+			this.NOTI_SUP = value;
+			this._flagsChanged = true;
+		}
+	}
+	private bool NOTI_SUP { get; set; }
 
 	/// <summary>
 	/// Whether flags were changed.
