c34cbb138cba2e90d954772fff9809bb16ca9432
Lala Sabathil <aiko@aitsys.dev>
2023-05-07 20:52:21 +0200

    Docfx fix again (#297)
    * testing
    
    * Update docfx.json
    
    * Update documentation_test.yml
    
    * Update documentation_test.yml
    
    * fix docs
    
    ---------
    
    Co-authored-by: NyuwBot <nyuw@aitsys.dev>

--- DisCatSharp.Docs/dcs/public/main.js
+++ DisCatSharp.Docs/dcs/public/main.js
@@ -1,2 +1,19 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+$(function() {
+    $('table').each(function(a, tbl) {
+        var currentTableRows = $(tbl).find('tbody tr').length;
+        $(tbl).find('th').each(function(i) {
+            var remove = 0;
+            var currentTable = $(this).parents('table');
+
+            var tds = currentTable.find('tr td:nth-child(' + (i + 1) + ')');
+            tds.each(function(j) { if ($(this).text().trim() === '') remove++; });
+
+            if (remove == currentTableRows) {
+                $(this).hide();
+                tds.hide();
+            }
+        });
+    });
+});
