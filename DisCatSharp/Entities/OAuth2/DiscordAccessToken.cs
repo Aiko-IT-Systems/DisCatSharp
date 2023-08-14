7079faeb893c042254c86a8aad8f1582140a7473
Lala Sabathil <aiko@aitsys.dev>
2023-08-15 01:17:12 +0200

    feat: DiscordAccessToken.FromJson

--- DisCatSharp/Entities/OAuth2/DiscordAccessToken.cs
+++ DisCatSharp/Entities/OAuth2/DiscordAccessToken.cs
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+using DisCatSharp.Net.Serialization;
+
 using Newtonsoft.Json;
 
 namespace DisCatSharp.Entities.OAuth2;
@@ -58,4 +60,34 @@ public sealed class DiscordAccessToken : ObservableApiObject
 	/// </summary>
 	[JsonProperty("scope")]
 	public string Scope { get; internal set; }
+
+	/// <summary>
+	/// Constructs a new <see cref="DiscordAccessToken"/> for usage with <see cref="DiscordOAuth2Client"/>.
+	/// </summary>
+	/// <param name="accessToken">The access token.</param>
+	/// <param name="tokenType">The token type.</param>
+	/// <param name="expiresIn">When the token expires.</param>
+	/// <param name="refreshToken">The refresh token.</param>
+	/// <param name="scope">The scope(s).</param>
+	public DiscordAccessToken(string accessToken, string tokenType, int expiresIn, string refreshToken, string scope)
+	{
+		this.AccessToken = accessToken;
+		this.TokenType = tokenType;
+		this.ExpiresIn = expiresIn;
+		this.RefreshToken = refreshToken;
+		this.Scope = scope;
+	}
+
+	/// <summary>
+	/// Constructs a new <see cref="DiscordAccessToken"/>.
+	/// </summary>
+	internal DiscordAccessToken()
+	{ }
+
+	/// <summary>
+	/// Generates a new <see cref="DiscordAccessToken"/> from json.
+	/// </summary>
+	/// <param name="json">The json.</param>
+	public static DiscordAccessToken FromJson(string json)
+		=> DiscordJson.DeserializeObject<DiscordAccessToken>(json, null);
 }
