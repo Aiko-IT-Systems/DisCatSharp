420a381301072919520b5aff946d3d1bd38861ad
Mira <56395159+TheXorog@users.noreply.github.com>
2023-10-17 18:00:09 +0200

    feat: AutoModerationRuleDeleteChangeSet

--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/AutoModerationRuleDeleteChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/AutoModerationRuleDeleteChangeSet.cs
@@ -1,7 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using DisCatSharp.Enums;
+
 namespace DisCatSharp.Entities;
 
 /// <summary>
 /// Represents a change set for deleting an Auto Moderation rule.
 /// </summary>
 public class AutoModerationRuleDeleteChangeSet : DiscordAuditLogEntry
-{ }
+{
+	internal AutoModerationRuleDeleteChangeSet()
+	{
+		this.ValidFor = AuditLogActionType.AutoModerationRuleDelete;
+	}
+
+	public bool Enabled => (bool)this.Changes.FirstOrDefault(x => x.Key == "enabled")?.OldValue;
+	public string RuleName => (string)this.Changes.FirstOrDefault(x => x.Key == "name")?.OldValue;
+	public AutomodTriggerType TriggerType => (AutomodTriggerType)this.Changes.FirstOrDefault(x => x.Key == "trigger_type")?.OldValue;
+	public AutomodEventType EventType => (AutomodEventType)this.Changes.FirstOrDefault(x => x.Key == "event_type")?.OldValue;
+	public AutomodAction[] Actions => (AutomodAction[])this.Changes.FirstOrDefault(x => x.Key == "actions")?.OldValue;
+	public AutomodTriggerMetadata TriggerMetadata => (AutomodTriggerMetadata)this.Changes.FirstOrDefault(x => x.Key == "trigger_metadata")?.OldValue;
+
+	public IReadOnlyList<ulong>? ExemptRoleIds => ((IReadOnlyList<string>?)this.Changes.FirstOrDefault(x => x.Key == "exempt_roles")?.OldValue)?.Select(x => ConvertToUlong(x)!.Value).ToList();
+	public IEnumerable<DiscordRole> ExemptRoles => this.ExemptRoles.Select(x => this.Discord.Guilds[this.GuildId].Roles[x.Id]);
+
+	public IReadOnlyList<ulong>? ExemptChannelIds => ((IReadOnlyList<string>?)this.Changes.FirstOrDefault(x => x.Key == "exempt_channels")?.OldValue)?.Select(x => ConvertToUlong(x)!.Value).ToList();
+	public IEnumerable<DiscordChannel> ExemptChannels => this.ExemptChannels.Select(x => this.Discord.Guilds[this.GuildId].Channels[x.Id]);
+}
