af17cf488e3f7a233a6e0e44a61f37dadc4909c0
Mira <56395159+TheXorog@users.noreply.github.com>
2023-10-17 18:27:39 +0200

    feat: AutoModerationUserCommunicationDisabledChangeSet

--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/AutoModerationUserCommunicationDisabledChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/AutoModerationUserCommunicationDisabledChangeSet.cs
@@ -1,7 +1,34 @@
-﻿namespace DisCatSharp.Entities;
+using DisCatSharp.Enums;
+
+namespace DisCatSharp.Entities;
 
 /// <summary>
 /// Represents a change set for a member being timed out by Auto Moderation.
 /// </summary>
 public class AutoModerationUserCommunicationDisabledChangeSet : DiscordAuditLogEntry
-{ }
+{
+	internal AutoModerationUserCommunicationDisabledChangeSet()
+	{
+		this.ValidFor = AuditLogActionType.AutoModerationUserCommunicationDisabled;
+	}
+
+	/// <summary>
+	/// The rule's name that blocked the message and triggered the timeout.
+	/// </summary>
+	public string RuleName => this.Options.AutoModerationRuleName;
+
+	/// <summary>
+	/// What type of filter the rule uses.
+	/// </summary>
+	public AutomodTriggerType? TriggerType => this.Options.AutoModerationRuleTriggerType;
+
+	/// <summary>
+	/// The channel's id in which the rule triggered.
+	/// </summary>
+	public ulong? ChannelId => this.Options.ChannelId!.Value;
+
+	/// <summary>
+	/// The channel in which the rule triggered.
+	/// </summary>
+	public DiscordChannel Channel => this.Options.Channel;
+}
