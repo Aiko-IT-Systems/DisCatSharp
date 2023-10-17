572b8e143b728bc22e09f099ee1832111d58a862
Mira <56395159+TheXorog@users.noreply.github.com>
2023-10-17 17:36:22 +0200

    feat: docs

--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/AutoModerationBlockMessageChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/AutoModerationBlockMessageChangeSet.cs
@@ -5,16 +5,30 @@ namespace DisCatSharp.Entities;
 /// <summary>
 /// Represents a change set for a message being blocked by Auto Moderation.
 /// </summary>
-public class AutoModerationBlockMessageChangeSet : DiscordAuditLogEntry
+public sealed class AutoModerationBlockMessageChangeSet : DiscordAuditLogEntry
 {
-	public AutoModerationBlockMessageChangeSet()
+	internal AutoModerationBlockMessageChangeSet()
 	{
 		this.ValidFor = AuditLogActionType.AutoModerationBlockMessage;
 	}
 
+	/// <summary>
+	/// The rule's name that blocked the message.
+	/// </summary>
 	public string RuleName => this.Options.AutoModerationRuleName;
+
+	/// <summary>
+	/// What type of filter the rule uses.
+	/// </summary>
 	public AutomodTriggerType? TriggerType => this.Options.AutoModerationRuleTriggerType;
 
-	public ulong ChannelId => this.Options.ChannelId!.Value;
+	/// <summary>
+	/// The channel's id in which the rule triggered.
+	/// </summary>
+	public ulong? ChannelId => this.Options.ChannelId!.Value;
+
+	/// <summary>
+	/// The channel in which the rule triggered.
+	/// </summary>
 	public DiscordChannel Channel => this.Options.Channel;
 }
