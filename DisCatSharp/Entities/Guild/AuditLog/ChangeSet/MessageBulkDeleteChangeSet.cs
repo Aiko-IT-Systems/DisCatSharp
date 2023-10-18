f7aac861448f95a07204a7a02f084e5e23aae2da
Mira <56395159+TheXorog@users.noreply.github.com>
2023-10-18 18:24:22 +0200

    feat: Message Deletions

--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MessageBulkDeleteChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MessageBulkDeleteChangeSet.cs
@@ -1,7 +1,18 @@
+using DisCatSharp.Enums;
+
 namespace DisCatSharp.Entities;
 
 /// <summary>
 /// Represents a change set for deleting multiple messages.
 /// </summary>
 public sealed class MessageBulkDeleteChangeSet : DiscordAuditLogEntry
-{ }
+{
+	internal MessageBulkDeleteChangeSet()
+	{
+		this.ValidFor = AuditLogActionType.MessageBulkDelete;
+	}
+
+	public DiscordChannel Channel => this.Discord.Guilds[this.GuildId].Channels[this.TargetId!.Value];
+
+	public int Count => (int)this.Options.Count;
+}
--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MessageDeleteChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MessageDeleteChangeSet.cs
@@ -1,7 +1,21 @@
+using DisCatSharp.Enums;
+
 namespace DisCatSharp.Entities;
 
 /// <summary>
 /// Represents a change set for deleting a single message.
 /// </summary>
 public sealed class MessageDeleteChangeSet : DiscordAuditLogEntry
-{ }
+{
+	internal MessageDeleteChangeSet()
+	{
+		this.ValidFor = AuditLogActionType.MessageDelete;
+	}
+
+	public DiscordUser Target => this.Discord.GetCachedOrEmptyUserInternal(this.TargetId!.Value);
+
+	public int Count => (int)this.Options.Count;
+
+	public ulong ChannelId => (ulong)this.Options.ChannelId;
+	public DiscordChannel? Channel => this.Discord.Guilds[this.GuildId].Channels.TryGetValue(this.ChannelId, out var channel) ? channel : null;
+}
