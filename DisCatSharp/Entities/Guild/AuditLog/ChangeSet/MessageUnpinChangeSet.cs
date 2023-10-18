b2bc879fabef4fc54df79862aa6e8dc6891ecf05
Mira <56395159+TheXorog@users.noreply.github.com>
2023-10-18 18:53:35 +0200

    feat: MessageUnpinChangeSet

--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MessageUnpinChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MessageUnpinChangeSet.cs
@@ -1,7 +1,22 @@
+using DisCatSharp.Enums;
+
 namespace DisCatSharp.Entities;
 
 /// <summary>
 /// Represents a change set for unpinning a message from a channel.
 /// </summary>
 public sealed class MessageUnpinChangeSet : DiscordAuditLogEntry
-{ }
+{
+	public MessageUnpinChangeSet()
+	{
+		this.ValidFor = AuditLogActionType.MessageUnpin;
+	}
+
+	public DiscordUser Target => this.Discord.GetCachedOrEmptyUserInternal(this.TargetId!.Value);
+
+	public ulong ChannelId => (ulong)this.Options.ChannelId;
+	public DiscordChannel? Channel => this.Discord.Guilds[this.GuildId].Channels.TryGetValue(this.ChannelId, out var channel) ? channel : null;
+
+	public ulong MessageId => (ulong)this.Options.MessageId;
+	public DiscordMessage? Message => this.Discord.MessageCache.TryGet(x => x.Id == this.MessageId, out var message) ? message : null;
+}
