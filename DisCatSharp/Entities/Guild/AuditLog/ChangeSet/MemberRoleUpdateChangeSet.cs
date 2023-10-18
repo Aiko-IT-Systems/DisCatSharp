06a816f262fe77a0f88c7ce35c1eaca1fe6e2db8
Mira <56395159+TheXorog@users.noreply.github.com>
2023-10-18 18:07:39 +0200

    feat: MemberRoleUpdateChangeSet

--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberRoleUpdateChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberRoleUpdateChangeSet.cs
@@ -1,7 +1,25 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using DisCatSharp.Enums;
+
 namespace DisCatSharp.Entities;
 
 /// <summary>
 /// Represents a change set for adding or removing a role from a member.
 /// </summary>
 public sealed class MemberRoleUpdateChangeSet : DiscordAuditLogEntry
-{ }
+{
+	internal MemberRoleUpdateChangeSet()
+	{
+		this.ValidFor = AuditLogActionType.MemberRoleUpdate;
+	}
+
+	public IReadOnlyList<DiscordRole> AddedRoles
+		=> ((IReadOnlyList<DiscordRole>)this.Changes.FirstOrDefault(x => x.Key == "$add")?.OldValue)
+				.Select(x => this.Discord.Guilds[this.GuildId].GetRole(x.Id)).ToList();
+
+	public IReadOnlyList<DiscordRole> RemovedRoles
+		=> ((IReadOnlyList<DiscordRole>)this.Changes.FirstOrDefault(x => x.Key == "$remove")?.OldValue)
+				.Select(x => this.Discord.Guilds[this.GuildId].GetRole(x.Id)).ToList();
+}
