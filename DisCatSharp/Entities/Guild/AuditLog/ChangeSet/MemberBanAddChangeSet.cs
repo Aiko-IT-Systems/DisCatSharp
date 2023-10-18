bd6c4dd99904e3ff95a0781e46565816b2dd18cc
Mira <56395159+TheXorog@users.noreply.github.com>
2023-10-18 17:43:04 +0200

    feat: Bans

--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberBanAddChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberBanAddChangeSet.cs
@@ -1,7 +1,16 @@
+using DisCatSharp.Enums;
+
 namespace DisCatSharp.Entities;
 
 /// <summary>
 /// Represents a change set for banning a member from the server.
 /// </summary>
 public sealed class MemberBanAddChangeSet : DiscordAuditLogEntry
-{ }
+{
+	internal MemberBanAddChangeSet()
+	{
+		this.ValidFor = AuditLogActionType.MemberBanAdd;
+	}
+
+	public DiscordUser Target => this.Discord.GetCachedOrEmptyUserInternal(this.TargetId!.Value);
+}
--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberBanRemoveChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberBanRemoveChangeSet.cs
@@ -1,7 +1,16 @@
+using DisCatSharp.Enums;
+
 namespace DisCatSharp.Entities;
 
 /// <summary>
 /// Represents a change set for lifting a server ban for a member.
 /// </summary>
 public sealed class MemberBanRemoveChangeSet : DiscordAuditLogEntry
-{ }
+{
+	internal MemberBanRemoveChangeSet()
+	{
+		this.ValidFor = AuditLogActionType.MemberBanRemove;
+	}
+
+	public DiscordUser Target => this.Discord.GetCachedOrEmptyUserInternal(this.TargetId!.Value);
+}
