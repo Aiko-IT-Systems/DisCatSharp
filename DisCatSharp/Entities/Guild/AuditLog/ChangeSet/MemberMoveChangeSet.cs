bc254810f3a082a02e6923d6ed9f567b094337cc
Mira <56395159+TheXorog@users.noreply.github.com>
2023-10-18 17:49:11 +0200

    feat: MemberMoveChangeSet

--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberMoveChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberMoveChangeSet.cs
@@ -1,7 +1,21 @@
+using System.Linq;
+
+using DisCatSharp.Enums;
+
 namespace DisCatSharp.Entities;
 
 /// <summary>
 /// Represents a change set for moving a member to a different voice channel.
 /// </summary>
 public sealed class MemberMoveChangeSet : DiscordAuditLogEntry
-{ }
+{
+	internal MemberMoveChangeSet()
+	{
+		this.ValidFor = AuditLogActionType.MemberMove;
+	}
+
+	public int Count => (int)this.Changes.FirstOrDefault(x => x.Key == "count")?.OldValue;
+
+	public ulong ChannelId => (ulong)this.Changes.FirstOrDefault(x => x.Key == "channel_id")?.OldValue;
+	public DiscordChannel? Channel => this.Discord.Guilds[this.GuildId].Channels.TryGetValue(this.ChannelId, out var channel) ? channel : null;
+}
