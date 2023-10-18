cfd43a3d48fa892ae93219596e15bd808e3c5dec
Mira <56395159+TheXorog@users.noreply.github.com>
2023-10-18 17:45:15 +0200

    feat: MemberDisconnectChangeSet

--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberDisconnectChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberDisconnectChangeSet.cs
@@ -1,7 +1,18 @@
+using System.Linq;
+
+using DisCatSharp.Enums;
+
 namespace DisCatSharp.Entities;
 
 /// <summary>
 /// Represents a change set for disconnecting a member from a voice channel.
 /// </summary>
 public sealed class MemberDisconnectChangeSet : DiscordAuditLogEntry
-{ }
+{
+	internal MemberDisconnectChangeSet()
+	{
+		this.ValidFor = AuditLogActionType.MemberDisconnect;
+	}
+
+	public int Count => (int)this.Changes.FirstOrDefault(x => x.Key == "count")?.OldValue;
+}
