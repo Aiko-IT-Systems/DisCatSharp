87d8202579695585ae27c61e1ecffb354c240666
Mira <56395159+TheXorog@users.noreply.github.com>
2023-10-18 17:46:18 +0200

    feat: MemberKickChangeSet

--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberKickChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberKickChangeSet.cs
@@ -1,7 +1,16 @@
+using DisCatSharp.Enums;
+
 namespace DisCatSharp.Entities;
 
 /// <summary>
 /// Represents a change set for removing a member from the server.
 /// </summary>
 public sealed class MemberKickChangeSet : DiscordAuditLogEntry
-{ }
+{
+	internal MemberKickChangeSet()
+	{
+		this.ValidFor = AuditLogActionType.MemberKick;
+	}
+
+	public DiscordUser Target => this.Discord.GetCachedOrEmptyUserInternal(this.TargetId!.Value);
+}
