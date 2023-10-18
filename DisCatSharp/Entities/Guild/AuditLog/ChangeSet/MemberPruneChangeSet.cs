3dc9165a73a4a4efd615ceea862f04f151a48e9e
Mira <56395159+TheXorog@users.noreply.github.com>
2023-10-18 18:01:00 +0200

    feat: MemberPruneChangeSet

--- DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberPruneChangeSet.cs
+++ DisCatSharp/Entities/Guild/AuditLog/ChangeSet/MemberPruneChangeSet.cs
@@ -1,7 +1,19 @@
+using System.Linq;
+
+using DisCatSharp.Enums;
+
 namespace DisCatSharp.Entities;
 
 /// <summary>
 /// Represents a change set for pruning members from the server.
 /// </summary>
 public sealed class MemberPruneChangeSet : DiscordAuditLogEntry
-{ }
+{
+	internal MemberPruneChangeSet()
+	{
+		this.ValidFor = AuditLogActionType.MemberPrune;
+	}
+
+	public int DeleteMemberDays => (int)this.Changes.FirstOrDefault(x => x.Key == "delete_member_days")?.OldValue;
+	public int Count => (int)this.Changes.FirstOrDefault(x => x.Key == "members_removed")?.OldValue;
+}
