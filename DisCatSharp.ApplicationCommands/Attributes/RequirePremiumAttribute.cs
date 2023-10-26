79828ea8398b4fbb5a7219f2eaad33470e04200e
Lala Sabathil <aiko@aitsys.dev>
2023-10-26 03:36:14 +0200

    feat: improvement for premium apps
    - config sku and test sku id
    - config auto fetch sku ids
    - ApplicationCommandRequirePremiumAttribute - auto fetched
    - ApplicationCommandRequirePremiumAttribute(sku_id)
    - ApplicationCommandRequirePremiumTestAttribute - auto fetched
    - ApplicationCommandRequirePremiumTestAttribute (sku_id)

---
+++ DisCatSharp.ApplicationCommands/Attributes/RequirePremiumAttribute.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Threading.Tasks;
+
+using DisCatSharp.ApplicationCommands.Context;
+using DisCatSharp.Enums;
+
+namespace DisCatSharp.ApplicationCommands.Attributes;
+
+/// <summary>
+/// Defines that usage of this application command is restricted to users with a specified entitlement.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = false)]
+public sealed class ApplicationCommandRequirePremiumAttribute : ApplicationCommandCheckBaseAttribute
+{
+	/// <summary>
+	/// Gets the entitlement id required by this attribute.
+	/// </summary>
+	public ulong? EntitlementId { get; } = null;
+
+	/// <summary>
+	/// Defines that usage of this command is restricted to users with a specified entitlement.
+	/// </summary>
+	/// <param name="entitlementId">Entitlement id required to execute this command.</param>
+	public ApplicationCommandRequirePremiumAttribute(ulong entitlementId)
+	{
+		this.EntitlementId = entitlementId;
+	}
+
+	/// <summary>
+	/// Defines that usage of this command is restricted to users with a specified entitlement.
+	/// <para>Use this attribute if you set <see cref="DiscordConfiguration.SkuId"/> or <see cref="DiscordConfiguration.AutoFetchSkuId"/> in your &lt;see cref="DiscordConfiguration"/&gt;.</para>
+	/// </summary>
+	public ApplicationCommandRequirePremiumAttribute()
+	{ }
+
+	/// <summary>
+	/// Runs checks.
+	/// </summary>
+	public override async Task<bool> ExecuteChecksAsync(BaseContext ctx)
+	{
+		var targetSkuId = this.EntitlementId ?? ctx.Client.Configuration.SkuId ?? throw new("Missing SKU ID");
+
+		if (ctx.Interaction.EntitlementSkuIds.Contains(targetSkuId))
+			return await Task.FromResult(true).ConfigureAwait(false);
+
+		await ctx.CreateResponseAsync(InteractionResponseType.PremiumRequired).ConfigureAwait(false);
+		return await Task.FromResult(false).ConfigureAwait(false);
+	}
+}
+
+/// <summary>
+/// Defines that usage of this application command is restricted to users with a test entitlement.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = false)]
+public sealed class ApplicationCommandRequirePremiumTestAttribute : ApplicationCommandCheckBaseAttribute
+{
+	/// <summary>
+	/// Gets the entitlement id required by this attribute.
+	/// </summary>
+	public ulong? EntitlementId { get; } = null;
+
+	/// <summary>
+	/// Defines that usage of this command is restricted to users with a specified entitlement.
+	/// </summary>
+	/// <param name="entitlementId">Entitlement id required to execute this command.</param>
+	public ApplicationCommandRequirePremiumTestAttribute(ulong entitlementId)
+	{
+		this.EntitlementId = entitlementId;
+	}
+
+	/// <summary>
+	/// Defines that usage of this command is restricted to users with a specified entitlement.
+	/// <para>Use this attribute if you set <see cref="DiscordConfiguration.TestSkuId"/> or <see cref="DiscordConfiguration.AutoFetchSkuId"/> in your <see cref="DiscordConfiguration"/>.</para>
+	/// </summary>
+	public ApplicationCommandRequirePremiumTestAttribute()
+	{ }
+
+	/// <summary>
+	/// Runs checks.
+	/// </summary>
+	public override async Task<bool> ExecuteChecksAsync(BaseContext ctx)
+	{
+		var targetSkuId = this.EntitlementId ?? ctx.Client.Configuration.TestSkuId ?? throw new("Missing SKU ID");
+
+		if (ctx.Interaction.EntitlementSkuIds.Contains(targetSkuId))
+			return await Task.FromResult(true).ConfigureAwait(false);
+
+		await ctx.CreateResponseAsync(InteractionResponseType.PremiumRequired).ConfigureAwait(false);
+		return await Task.FromResult(false).ConfigureAwait(false);
+	}
+}
