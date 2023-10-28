6e13a33d06b331f7c86197ddd3107e54629c1fc9
Lala Sabathil <aiko@aitsys.dev>
2023-10-28 23:12:32 +0200

    feat!: flagsssssss

--- DisCatSharp/Enums/Application/ApplicationDiscoverabilityState.cs
+++ DisCatSharp/Enums/Application/ApplicationDiscoverabilityState.cs
@@ -6,9 +6,28 @@ namespace DisCatSharp.Enums;
 /// </summary>
 public enum ApplicationDiscoverabilityState : int
 {
+	/// <summary>
+	/// This application is ineligible for the application directory
+	/// </summary>
 	Ineligible = 1,
+
+	/// <summary>
+	/// This application is not listed in the application directory
+	/// </summary>
 	NotDiscoverable = 2,
+
+	/// <summary>
+	/// This application is listed in the application directory
+	/// </summary>
 	Discoverable = 3,
+
+	/// <summary>
+	/// This application is featurable in the application directory
+	/// </summary>
 	Featureable = 4,
+
+	/// <summary>
+	/// This application has been blocked from appearing in the application directory
+	/// </summary>
 	Blocked = 5
 }
---
+++ DisCatSharp/Enums/Application/ExplicitContentFilterLevel.cs
@@ -0,0 +1,18 @@
+
+namespace DisCatSharp.Enums;
+
+/// <summary>
+/// Represents the explicit content filter level.
+/// </summary>
+public enum ExplicitContentFilterLevel : int
+{
+	/// <summary>
+	/// Media content will not be scanned
+	/// </summary>
+	Disabled = 0,
+
+	/// <summary>
+	/// Media content will be scanned
+	/// </summary>
+	Enabled = 1
+}
