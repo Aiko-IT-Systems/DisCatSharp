f9396627249a4fd05c5e2ad7efed56b705432fc3
Lala Sabathil <aiko@aitsys.dev>
2023-10-28 23:12:32 +0200

    feat!: flagsssssss

---
+++ DisCatSharp/Enums/Application/ApplicationMonetizationState.cs
@@ -0,0 +1,23 @@
+
+namespace DisCatSharp.Enums;
+
+/// <summary>
+/// Represents the application discoverability state.
+/// </summary>
+public enum ApplicationMonetizationState : int
+{
+	/// <summary>
+	/// This application does not have monetization set up
+	/// </summary>
+	None = 1,
+
+	/// <summary>
+	/// This application has monetization set up
+	/// </summary>
+	Enabled = 2,
+
+	/// <summary>
+	/// This application has been blocked from monetizing
+	/// </summary>
+	Blocked = 3
+}
