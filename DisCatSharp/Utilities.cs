e71710751a03e7ea4080cd66332a88da85f6aa2c
Lala Sabathil <aiko@aitsys.dev>
2023-12-30 18:24:35 +0100

    fix!: gateway compression, encoding and version was never applied
    thanks dzi, honestly
    
    Co-Authored-By: Dziurwa <54191536+dziurwa14@users.noreply.github.com>

--- DisCatSharp/Utilities.cs
+++ DisCatSharp/Utilities.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 
 using DisCatSharp.Common;
 using DisCatSharp.Common.RegularExpressions;
@@ -87,6 +88,23 @@ public static class Utilities
 		}
 
 		VersionHeader = $"DiscordBot (https://github.com/Aiko-IT-Systems/DisCatSharp, v{vs})";
+	}
+
+	/// <summary>
+	/// Adds the specified parameter to the Query String.
+	/// </summary>
+	/// <param name="url"></param>
+	/// <param name="paramName">Name of the parameter to add.</param>
+	/// <param name="paramValue">Value for the parameter to add.</param>
+	/// <returns>Url with added parameter.</returns>
+	public static Uri AddParameter(this Uri url, string paramName, string paramValue)
+	{
+		var uriBuilder = new UriBuilder(url);
+		var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+		query[paramName] = paramValue;
+		uriBuilder.Query = query.ToString();
+
+		return uriBuilder.Uri;
 	}
 
 	/// <summary>
