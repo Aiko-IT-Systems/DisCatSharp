ae66488a4cda2ed1c12fcb6e2301560b81d494c2
Lala Sabathil <aiko@aitsys.dev>
2024-01-05 01:25:13 +0100

    feat: more named capture groups
    include staging.discord.co in regex

--- DisCatSharp.Common/RegularExpressions/DiscordRegEx.cs
+++ DisCatSharp.Common/RegularExpressions/DiscordRegEx.cs
@@ -7,10 +7,10 @@ namespace DisCatSharp.Common.RegularExpressions;
 /// </summary>
 public static partial class DiscordRegEx
 {
-	[GeneratedRegex(@"^((https?:\/\/)?(www\.)?discord\.gg(\/.*)*|(https?:\/\/)?(www\.|canary\.|ptb\.)?(discord|discordapp)\.com\/invite)\/(?<code>[a-zA-Z0-9]*)(\?event=(?<event>\d+))?$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
+	[GeneratedRegex(@"^((https?:\/\/)?(www\.)?discord\.gg(\/.*)*|(https?:\/\/)?(www\.|canary\.|ptb\.|staging\.)?(discord|discordapp)(\.com|\.co)\/invite)\/(?<code>[a-zA-Z0-9]*)(\?event=(?<event>\d+))?$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
 	public static partial Regex InviteRegex();
 
-	[GeneratedRegex(@"^(https?:\/\/)?(www\.|canary\.|ptb\.)?(discord|discordapp)\.com\/channels\/(?<guild>(?:\d+|@me))\/(?<channel>\d+)\/(?<message>\d+)\/?", RegexOptions.Compiled | RegexOptions.ECMAScript)]
+	[GeneratedRegex(@"^(https?:\/\/)?(www\.|canary\.|ptb\.|staging\.)?(discord|discordapp)(\.com|\.co)\/channels\/(?<guild>(?:\d+|@me))\/(?<channel>\d+)\/(?<message>\d+)\/?", RegexOptions.Compiled | RegexOptions.ECMAScript)]
 	public static partial Regex MessageLinkRegex();
 
 	[GeneratedRegex("^<(?<animated>a)?:(?<name>[a-zA-Z0-9_]+?):(?<id>\\d+?)>$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
@@ -31,13 +31,13 @@ public static partial class DiscordRegEx
 	[GeneratedRegex("^<t:(?<timestamp>-?\\d{1,13}):(?<style>[tTdDfFR])>$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
 	public static partial Regex StyledTimestampRegex();
 
-	[GeneratedRegex("^<@&(\\d+?)>$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
+	[GeneratedRegex("^<@&(?<id>\\d+)>$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
 	public static partial Regex RoleRegex();
 
-	[GeneratedRegex("^<#(\\d+)>$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
+	[GeneratedRegex("^<#(?<id>\\d+)>$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
 	public static partial Regex ChannelRegex();
 
-	[GeneratedRegex(@"^<@\!?(\d+?)>$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
+	[GeneratedRegex(@"^<@\!?(?<id>\d+?)>$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
 	public static partial Regex UserRegex();
 
 	[GeneratedRegex(@"^<@\!(?<id>\d{17,20})>$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
@@ -49,12 +49,15 @@ public static partial class DiscordRegEx
 	[GeneratedRegex("^<@(?<id>\\d{17,20})>$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
 	public static partial Regex UserWithoutNicknameRegex();
 
-	[GeneratedRegex(@"^(https?:\/\/)?(www\.|canary\.|ptb\.)?(discord|discordapp)\.com\/events\/(?<guild>\d+)\/(?<event>\d+)$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
+	[GeneratedRegex(@"^(https?:\/\/)?(www\.|canary\.|ptb\.|staging\.)?(discord|discordapp)(\.com|\.co)\/events\/(?<guild>\d+)\/(?<event>\d+)$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
 	public static partial Regex EventRegex();
 
 	[GeneratedRegex(@"^[\p{Ll}\p{Lt}\w_-]{1,32}$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
 	public static partial Regex ApplicationCommandNameRegex();
 
-	[GeneratedRegex(@"(?:https?:\/\/)?discord(?:app)?.com\/api\/(?:v\d\/)?webhooks\/(?<id>\d+)\/(?<token>[A-Za-z0-9_\-]+)", RegexOptions.Compiled | RegexOptions.ECMAScript)]
+	[GeneratedRegex(@"(?:https?:\/\/)?(www\.|canary\.|ptb\.|staging\.)?(discord|discordapp)(\.com|\.co)\/api\/(?:v\d\/)?webhooks\/(?<id>\d+)\/(?<token>[A-Za-z0-9_\-]+)", RegexOptions.Compiled | RegexOptions.ECMAScript)]
 	public static partial Regex WebhookRegex();
+
+	[GeneratedRegex("^<sound:(?<id>(\\d+))>$", RegexOptions.Compiled | RegexOptions.ECMAScript)]
+	public static partial Regex SoundRegex();
 }
