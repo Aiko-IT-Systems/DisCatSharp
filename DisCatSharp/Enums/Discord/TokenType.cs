6146d69de51c44f5e29c3034d9ac00517138a345
Lala Sabathil <aiko@aitsys.dev>
2024-01-26 16:28:33 +0100

    fix: fix app command extension and remove tokentype.user completely

--- DisCatSharp/Enums/Discord/TokenType.cs
+++ DisCatSharp/Enums/Discord/TokenType.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace DisCatSharp.Enums;
 
 /// <summary>
@@ -7,13 +5,6 @@ namespace DisCatSharp.Enums;
 /// </summary>
 public enum TokenType
 {
-	/// <summary>
-	/// User token type
-	/// </summary>
-	[Obsolete("Logging in with a user token may result in your account being terminated, and is therefore highly unrecommended." +
-	          "\nIf anything goes wrong with this, we will not provide any support!", true)]
-	User = 0,
-
 	/// <summary>
 	/// Bot token type
 	/// </summary>
