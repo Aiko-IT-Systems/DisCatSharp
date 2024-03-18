34d8dab5cd5a021fe9ac9680fbf0b8024ec981f9
LaPepega <tmv03092008@gmail.com>
2024-03-18 19:59:25 +0500

    Added missing "default_forum_layout" property for DiscordChannel

--- DisCatSharp/Entities/Channel/DiscordChannel.cs
+++ DisCatSharp/Entities/Channel/DiscordChannel.cs
@@ -235,6 +235,12 @@ public class DiscordChannel : SnowflakeObject, IEquatable<DiscordChannel>
 	public ForumPostSortOrder? DefaultSortOrder { get; internal set; }
 
 	/// <summary>
+	/// Gets the default forum layout for this channel
+	/// </summary>
+	[JsonProperty("default_forum_layout", NullValueHandling = NullValueHandling.Ignore)]
+	public ForumLayout? DefaultForumLayout { get; internal set; }
+
+	/// <summary>
 	/// Gets when the last pinned message was pinned.
 	/// </summary>
 	[JsonIgnore]
