d4eba35d77efcf700b644b8451b89feeed863687
Lala Sabathil <aiko@aitsys.dev>
2024-04-24 22:44:18 +0200

    docs: fix docs for entitlement type. todo: new routes

--- DisCatSharp/Enums/Store/EntitlementType.cs
+++ DisCatSharp/Enums/Store/EntitlementType.cs
@@ -1,16 +1,47 @@
 namespace DisCatSharp.Enums;
 
 /// <summary>
-/// Represents the test entitlement owner type.
+/// Represents the entitlement type.
 /// </summary>
 public enum EntitlementType
 {
+	/// <summary>
+	/// Entitlement was purchased by user.
+	/// </summary>
 	Purchase = 1,
+
+	/// <summary>
+	/// Entitlement for Discord Nitro subscription.
+	/// </summary>
 	PremiumSubscription = 2,
+
+	/// <summary>
+	/// Entitlement was gifted by developer.
+	/// </summary>
 	DeveloperGift = 3,
+
+	/// <summary>
+	/// Entitlement was purchased by a dev in application test mode.
+	/// </summary>
 	TestModePurchase = 4,
+
+	/// <summary>
+	/// Entitlement was granted when the SKU was free.
+	/// </summary>
 	FreePurchase = 5,
+
+	/// <summary>
+	/// Entitlement was gifted by another user.
+	/// </summary>
 	UserGift = 6,
+
+	/// <summary>
+	/// Entitlement was claimed by user for free as a Nitro Subscriber.
+	/// </summary>
 	PremiumPurchase = 7,
+
+	/// <summary>
+	/// Entitlement was purchased as an app subscription.
+	/// </summary>
 	ApplicationSubscription = 8
 }
