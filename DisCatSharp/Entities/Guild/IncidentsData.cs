46ca6b3e55ca0c58f1d6f125feac41884cf19cd6
Lala Sabathil <aiko@aitsys.dev>
2024-04-26 17:18:13 +0200

    feat: add raid and dm_spam detected to incident data

--- DisCatSharp/Entities/Guild/IncidentsData.cs
+++ DisCatSharp/Entities/Guild/IncidentsData.cs
@@ -10,16 +10,27 @@ namespace DisCatSharp.Entities;
 public class IncidentsData : ObservableApiObject
 {
 	/// <summary>
+	/// Gets until when direct messages are disabled.
+	/// </summary>
+	[JsonProperty("dms_disabled_until", NullValueHandling = NullValueHandling.Include)]
+	public DateTimeOffset? DmsDisabledUntil { get; internal set; }
+
+	/// <summary>
 	/// Gets until when invites are disabled.
 	/// </summary>
 	[JsonProperty("invites_disabled_until", NullValueHandling = NullValueHandling.Include)]
 	public DateTimeOffset? InvitesDisabledUntil { get; internal set; }
 
 	/// <summary>
-	/// Gets until when direct messages are disabled.
+	/// Gets when the dm spam was detected at.
 	/// </summary>
-	[JsonProperty("dms_disabled_until", NullValueHandling = NullValueHandling.Include)]
-	public DateTimeOffset? DmsDisabledUntil { get; internal set; }
+	[JsonProperty("dm_spam_detected_at", NullValueHandling = NullValueHandling.Include)]
+	public DateTimeOffset? DmSpamDetectedAt { get; internal set; }
+	/// <summary>
+	/// Gets when the raid was detected at.
+	/// </summary>
+	[JsonProperty("raid_detected_at", NullValueHandling = NullValueHandling.Include)]
+	public DateTimeOffset? RaidDetectedAt { get; internal set; }
 
 	/// <summary>
 	/// Constructs a new <see cref="IncidentsData"/> object.
