f11cfa126931ed4b09c3e5c64df827fcbfc22826
Mira <56395159+TheXorog@users.noreply.github.com>
2024-05-17 16:14:58 +0200

    fix regex to actually match tokens & simplify
    Co-authored-by: quinchs <49576606+quinchs@users.noreply.github.com>

--- DisCatSharp/Utilities.cs
+++ DisCatSharp/Utilities.cs
@@ -102,8 +102,7 @@ public static class Utilities
 		if (string.IsNullOrWhiteSpace(str))
 			return str;
 
-		str = Regex.Replace(str, @"([a-zA-Z0-9]{68})", "{WEBHOOK_TOKEN}");
-		str = Regex.Replace(str, @"^(.+):\d+?:[a-zA-Z0-9]{128}$", "{INTERACTION_TOKEN}");
+		str = Regex.Replace(str, @"([a-zA-Z0-9]{68,})", "{WEBHOOK_OR_INTERACTION_TOKEN}"); // Any alphanumeric string this long is likely to be sensitive information anyways
 		str = Regex.Replace(str, @"(mfa\.[a-z0-9_-]{20,})|((?<botid>[a-z0-9_-]{23,28})\.(?<creation>[a-z0-9_-]{6,7})\.(?<enc>[a-z0-9_-]{27,}))", "{ACCOUNT_TOKEN}");
 
 		return str;
