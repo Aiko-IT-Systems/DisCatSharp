5135a82608cf5bf24a472c1ce2c4b0a78b856428
Lala Sabathil <aiko@aitsys.dev>
2025-02-08 10:58:46 +0100

    feat: easy statistics!

---
+++ DisCatSharp/Enums/Core/DisCatSharpStatisticType.cs
@@ -0,0 +1,57 @@
+namespace DisCatSharp.Enums.Core;
+
+/// <summary>
+///     Represents the type of statistic.
+/// </summary>
+public enum DisCatSharpStatisticType
+{
+	/// <summary>
+	/// Represents the number of guilds.
+	/// </summary>
+	Guilds,
+
+	/// <summary>
+	/// Represents the number of users.
+	/// </summary>
+	Users,
+
+	/// <summary>
+	/// Represents the number of channels.
+	/// </summary>
+	Channels,
+
+	/// <summary>
+	/// Represents the number of threads.
+	/// </summary>
+	Threads,
+
+	/// <summary>
+	/// Represents the number of roles.
+	/// </summary>
+	Roles,
+
+	/// <summary>
+	/// Represents the number of emojis.
+	/// </summary>
+	Emojis,
+
+	/// <summary>
+	/// Represents the number of stickers.
+	/// </summary>
+	Stickers,
+
+	/// <summary>
+	/// Represents the number of soundboard sounds.
+	/// </summary>
+	SoundboardSounds,
+
+	/// <summary>
+	/// Represents the number of stage instances.
+	/// </summary>
+	StageInstances,
+
+	/// <summary>
+	/// Represents the number of scheduled events.
+	/// </summary>
+	ScheduledEvents
+}
