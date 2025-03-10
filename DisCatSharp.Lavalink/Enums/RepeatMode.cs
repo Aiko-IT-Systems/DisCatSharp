d9dd1f685f26c7c04791b715f6a9f7953f7ebf83
Lala Sabathil <aiko@aitsys.dev>
2025-03-10 10:09:51 +0100

    fox: file-scoped namespace

--- DisCatSharp.Lavalink/Enums/RepeatMode.cs
+++ DisCatSharp.Lavalink/Enums/RepeatMode.cs
@@ -1,23 +1,22 @@
-namespace DisCatSharp.Lavalink.Enums
+namespace DisCatSharp.Lavalink.Enums;
+
+/// <summary>
+/// Represents the repeat mode for the Lavalink player.
+/// </summary>
+public enum RepeatMode
 {
-    /// <summary>
-    /// Represents the repeat mode for the Lavalink player.
-    /// </summary>
-    public enum RepeatMode
-    {
-        /// <summary>
-        /// No repeat mode. The queue operates normally and does not repeat.
-        /// </summary>
-        None,
+	/// <summary>
+	/// No repeat mode. The queue operates normally and does not repeat.
+	/// </summary>
+	None,
 
-        /// <summary>
-        /// Repeat all mode. The entire queue repeats once it ends.
-        /// </summary>
-        All,
+	/// <summary>
+	/// Repeat all mode. The entire queue repeats once it ends.
+	/// </summary>
+	All,
 
-        /// <summary>
-        /// Repeat current mode. The current track is played over and over again.
-        /// </summary>
-        Current
-    }
+	/// <summary>
+	/// Repeat current mode. The current track is played over and over again.
+	/// </summary>
+	Current
 }
