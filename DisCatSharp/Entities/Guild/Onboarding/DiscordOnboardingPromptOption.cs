3121892c74c34e5e224e3c5d43476bcc8d6d5f59
Lala Sabathil <aiko@aitsys.dev>
2025-08-08 19:33:23 +0200

    Add emoji properties to onboarding prompt option
    Introduces EmojiName, EmojiId, and EmojiAnimated properties to DiscordOnboardingPromptOption for use in create and modify payloads. These properties expose the emoji's name, ID, and animated state, respectively.

--- DisCatSharp/Entities/Guild/Onboarding/DiscordOnboardingPromptOption.cs
+++ DisCatSharp/Entities/Guild/Onboarding/DiscordOnboardingPromptOption.cs
@@ -68,4 +68,25 @@ public sealed class DiscordOnboardingPromptOption : NullableSnowflakeObject
 	/// </summary>
 	[JsonProperty("channel_ids", NullValueHandling = NullValueHandling.Ignore)]
 	public List<ulong>? ChannelIds { get; internal set; }
+
+	/// <summary>
+	/// 	Sets the emoji name for create and modify payloads.
+	/// </summary>
+	[JsonProperty("emoji_name", NullValueHandling = NullValueHandling.Ignore)]
+	internal string? EmojiName
+		=> this.Emoji?.Name;
+
+	/// <summary>
+	///     Sets the emoji id for create and modify payloads.
+	/// </summary>
+	[JsonProperty("emoji_id", NullValueHandling = NullValueHandling.Ignore)]
+	internal ulong? EmojiId
+		=> this.Emoji?.Id;
+
+	/// <summary>
+	///     Sets the emoji animated state for create and modify payloads.
+	/// </summary>
+	[JsonProperty("emoji_animated", NullValueHandling = NullValueHandling.Ignore)]
+	internal bool? EmojiAnimated
+		=> this.Emoji?.IsAnimated;
 }
